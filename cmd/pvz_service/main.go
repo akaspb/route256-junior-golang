945d61645e8e280707f656c89bdb88ec427c59ea
Qefy <siralexpeter@mail.ru>
2024-10-19 16:48:16 +0300

    change options in main

--- cmd/pvz_service/main.go
+++ cmd/pvz_service/main.go
@@ -121,8 +121,6 @@ func initConfig() error {
 }
 
 func getPostgresDSN() string {
-	//postgres://username:password@host:port/database
-	//"postgres://postgres:postgres@localhost:5433/postgres?sslmode=disable"
 	return fmt.Sprintf(
 		"postgres://%s:%s@%s:%s/%s?sslmode=%s",
 		viper.GetString("db.username"),
@@ -137,15 +135,8 @@ func getPostgresDSN() string {
 
 func initProducer(config kafka.Config) (sarama.AsyncProducer, error) {
 	return kafka.NewAsyncProducer(config,
-		// kafka.WithIdempotent(),
-		kafka.WithRequiredAcks(sarama.WaitForAll),
-		// kafka.WithMaxOpenRequests(1),
 		kafka.WithMaxRetries(5),
 		kafka.WithRetryBackoff(10*time.Millisecond),
-		// kafka.WithProducerPartitioner(sarama.NewManualPartitioner),
-		// kafka.WithProducerPartitioner(sarama.NewRoundRobinPartitioner),
-		// kafka.WithProducerPartitioner(sarama.NewRandomPartitioner),
-		kafka.WithProducerPartitioner(sarama.NewHashPartitioner),
 		kafka.WithProducerFlushMessages(3),
 		kafka.WithProducerFlushFrequency(1*time.Second),
 	)
