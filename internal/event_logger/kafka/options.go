1ebb4208e26a1573598889a30c62239ad8312039
Qefy <siralexpeter@mail.ru>
2024-10-19 16:46:02 +0300

    change options setting

--- internal/event_logger/kafka/options.go
+++ internal/event_logger/kafka/options.go
@@ -17,7 +17,11 @@ func (fn optionFn) Apply(c *sarama.Config) error {
 	return fn(c)
 }
 
-// WithProducerPartitioner ...
+// WithProducerPartitioner - установить алгоритм выбор партиции
+//   - sarama.NewManualPartitioner - ручной
+//   - sarama.NewRandomPartitioner - случайная партиция
+//   - sarama.NewRoundRobinPartitioner - по кругу
+//   - sarama.NewHashPartitioner - по ключу
 func WithProducerPartitioner(pfn sarama.PartitionerConstructor) Option {
 	return optionFn(func(c *sarama.Config) error {
 		c.Producer.Partitioner = pfn
@@ -25,7 +29,10 @@ func WithProducerPartitioner(pfn sarama.PartitionerConstructor) Option {
 	})
 }
 
-// WithProducerPartitioner ...
+// WithRequiredAcks - установить параметр acks
+//   - sarama.NoResponse - ничего не ждем
+//   - sarama.WaitForLocal - ждем успешной записи ТОЛЬКО на лидер партиции
+//   - sarama.WaitForLocal - ждем успешной записи на лидер партиции и всех in-sync реплик (настроено в кафка кластере)
 func WithRequiredAcks(acks sarama.RequiredAcks) Option {
 	return optionFn(func(c *sarama.Config) error {
 		c.Producer.RequiredAcks = acks
@@ -33,7 +40,14 @@ func WithRequiredAcks(acks sarama.RequiredAcks) Option {
 	})
 }
 
-// WithIdempotent ...
+// WithIdempotent - установить семантики exactly once (установить Idempotent = true)
+/*
+	У продюсера есть счетчик (count).
+	Каждое успешно отправленное сообщение увеличивает счетчик (count++).
+	Если продюсер не смог отправить сообщение, то счетчик не меняется и отправляется в таком виде в другом сообщение.
+	Кафка это видит и начинает сравнивать (в том числе Key) сообщения с одинаковыми счетчиками.
+	Далее не дает отправить дубль, если Idempotent = true.
+*/
 func WithIdempotent() Option {
 	return optionFn(func(c *sarama.Config) error {
 		c.Producer.Idempotent = true
@@ -41,7 +55,7 @@ func WithIdempotent() Option {
 	})
 }
 
-// WithMaxRetries ...
+// WithMaxRetries - установить число попыток отправить сообщение
 func WithMaxRetries(n int) Option {
 	return optionFn(func(c *sarama.Config) error {
 		c.Producer.Retry.Max = n
@@ -49,7 +63,7 @@ func WithMaxRetries(n int) Option {
 	})
 }
 
-// WithRetryBackoff ...
+// WithRetryBackoff - установить интервалы между попытками отправить сообщение
 func WithRetryBackoff(d time.Duration) Option {
 	return optionFn(func(c *sarama.Config) error {
 		c.Producer.Retry.Backoff = d
@@ -57,7 +71,9 @@ func WithRetryBackoff(d time.Duration) Option {
 	})
 }
 
-// WithMaxOpenRequests ...
+// WithMaxOpenRequests - установить пропускную способность
+//
+// При значении 1 гарантируем строгий порядок отправки сообщений/батчей
 func WithMaxOpenRequests(n int) Option {
 	return optionFn(func(c *sarama.Config) error {
 		c.Net.MaxOpenRequests = n
@@ -65,7 +81,11 @@ func WithMaxOpenRequests(n int) Option {
 	})
 }
 
-// WithProducerFlushMessages ...
+// WithProducerFlushMessages - установить количество сообщений, которые должны быть собраны в очереди перед их отправкой
+//
+// Когда количество сообщений в очереди достигает этого значения, все собранные сообщения отправляются в брокеры.
+// Установка этого значения позволяет уменьшить количество вызовов к Kafka, что может существенно повысить производительность.
+// Вместо отправки каждого сообщения сразу, продюсер собирает несколько сообщений и отправляет их за одну операцию.
 func WithProducerFlushMessages(n int) Option {
 	return optionFn(func(c *sarama.Config) error {
 		c.Producer.Flush.Messages = n
@@ -73,7 +93,7 @@ func WithProducerFlushMessages(n int) Option {
 	})
 }
 
-// WithProducerFlushFrequency ...
+// WithProducerFlushFrequency - установить интервал времени, после которого все сообщения в очереди будут отправлены
 func WithProducerFlushFrequency(d time.Duration) Option {
 	return optionFn(func(c *sarama.Config) error {
 		c.Producer.Flush.Frequency = d
